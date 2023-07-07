896a87959a5193d290d4cd7dbd56a06b6fc04fdb
b1gcat <b1gcat@b1gcat.local>
2023-07-07 20:45:50 +0800

    update

--- utils/crypto.go
+++ utils/crypto.go
@@ -125,16 +125,3 @@ func split(buf []byte, lim int) [][]byte {
 	}
 	return chunks
 }
-
-func split(buf []byte, lim int) [][]byte {
-	var chunk []byte
-	chunks := make([][]byte, 0, len(buf)/lim+1)
-	for len(buf) >= lim {
-		chunk, buf = buf[:lim], buf[lim:]
-		chunks = append(chunks, chunk)
-	}
-	if len(buf) > 0 {
-		chunks = append(chunks, buf[:])
-	}
-	return chunks
-}
